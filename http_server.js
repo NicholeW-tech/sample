7f24a14195f5ea710935b24fc4a4b74ce833c8f5
kanwchan <kanwchan0@gmail.com>
2021-09-16 16:04:13 -0400

    test

--- http_server.js
+++ http_server.js
@@ -32,16 +32,7 @@ app.get('/data', function (req, res) {
 app.post('/add', function (req, res) {
     var user = {
         'name': req.body.name,
-        'dob': req.body.dob,
-        'email': req.body.email,
-        'username': req.body.username,
-        'password': req.body.password,
-        'phone': req.body.phone,
-        'streetaddress': req.body.streetaddress,
-        'citystatezip': req.body.citystatezip,
-        'latitude': req.body.latitude,
-        'longitude': req.body.longitude,
-        'avatar': req.body.avatar
+        
     }
     db.get('users').push(user).write();
     console.log(db.get('users').value());
